30ab37c794b8d7a6818fe4f636b2100d78a45508
Muli Spata <fspata20@epoka.edu.al>
2024-07-29 15:29:08 +0200

    Initial Commit

---
+++ src/components/icons/home-icon.tsx
@@ -0,0 +1,16 @@
+const HomeIcon = () => {
+  return (
+    <>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        width="24"
+        height="24"
+      >
+        <path d="M12 2L3 10V21a1 1 0 0 0 1 1h5v-6h4v6h5a1 1 0 0 0 1-1V10L12 2zm6 18h-4v-6H10v6H6V10.83L12 4l6 6.83V20z" />
+      </svg>
+    </>
+  );
+};
+
+export default HomeIcon;
---
+++ src/components/sidebar/index.tsx
@@ -0,0 +1,142 @@
+import { Link, useLocation } from "react-router-dom";
+import { Box, Hidden, Typography } from "@mui/material";
+import HomeIcon from "../icons/home-icon";
+import MovieIcon from "../icons/movie-icon";
+import TvSeriesIcon from "../icons/series-icon";
+import BookmarkIcon from "../icons/bookmark-icon";
+
+const navLinks = [
+  {
+    name: "Home",
+    icon: HomeIcon,
+    link: "/home",
+  },
+  {
+    name: "Login",
+    icon: HomeIcon,
+    link: "/",
+  },
+  {
+    name: "Movies",
+    icon: MovieIcon,
+    link: "/movie",
+  },
+  {
+    name: "TV Series",
+    icon: TvSeriesIcon,
+    link: "/tv-series",
+  },
+  {
+    name: "Bookmarks",
+    icon: BookmarkIcon,
+    link: "/bookmarks",
+  },
+];
+
+const Sidebar = () => {
+  const { pathname } = useLocation();
+
+  return (
+    <Box
+      sx={{
+        backgroundColor: "#161d2f",
+        padding: 2,
+        borderRadius: 2,
+        display: "flex",
+        flexDirection: {
+          xs: "row",
+          lg: "column",
+        },
+        alignItems: "center",
+        justifyContent: "space-between",
+        width: {
+          sm: "100%",
+          lg: 200,
+        },
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: {
+            xs: "row",
+            lg: "column",
+          },
+          gap: 5,
+          alignItems: {
+            xs: "center",
+            lg: "start",
+          },
+          width: "100%",
+        }}
+      >
+        <Hidden smDown>
+          <Typography
+            variant="h5"
+            component="h1"
+            my={2}
+            fontWeight={400}
+            fontSize={18}
+          >
+            PikaShowApp
+          </Typography>
+        </Hidden>
+
+        <Box
+          sx={{
+            py: {
+              xs: "0px",
+              lg: "16px",
+            },
+            display: "flex",
+            flexDirection: {
+              xs: "row",
+              lg: "column",
+            },
+            gap: 4,
+          }}
+        >
+          {navLinks.map((item) => (
+            <Link
+              key={item.name}
+              to={item.link}
+              style={{ textDecoration: "none" }}
+            >
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  gap: 2,
+                  color: "white",
+                  textDecoration: "none",
+                }}
+              >
+                {item.icon ? (
+                  <item.icon />
+                ) : (
+                  <img
+                    src={item.icon}
+                    alt={item.name}
+                    style={{
+                      width: "10px",
+                      filter: `${
+                        pathname === item.link
+                          ? "invert(58%) sepia(14%) saturate(3166%) hue-rotate(215deg) brightness(91%) contrast(87%)"
+                          : "invert(84%)"
+                      }`,
+                    }}
+                  />
+                )}
+                <Hidden mdDown>
+                  <Typography>{item.name}</Typography>
+                </Hidden>
+              </Box>
+            </Link>
+          ))}
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default Sidebar;
